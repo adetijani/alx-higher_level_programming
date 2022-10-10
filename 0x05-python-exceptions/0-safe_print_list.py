6bb2454aa87a4986f6025c55d4fab08a9bceede6
Debo <tijanidebo@gmail.com>
2022-10-10 21:11:55 +0100

    Exceptional Handler

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    cmpt = 0
-    for index in range(x):
+    counter = 0
+    for i in range(x):
         try:
-            print(my_list[index], end='')
-            cmpt += 1
-        except Exception as error:
-            break
-    print('')
-    return 
+            print("{}".format(my_list[i]), end="")
+            counter = counter + 1
+        except IndexError:
+            continue
+    print("")
+    return counter 
